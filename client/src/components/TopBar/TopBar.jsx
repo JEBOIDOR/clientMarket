2e9393f97af27697284ad0abcfea0e8ac094d8f0
JEBOIDOR <juenexplo@gmail.com>
2024-02-10 21:57:40 -0500

    mejora de ui

---
+++ client/src/components/TopBar/TopBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TopBar.css";
+import SearchBar from "../searchBar/SearchBar.jsx";
+import { Icon } from "@iconify/react";
+const TopBar = ({ toggleSidebar }) => {
+  return (
+    <div className="top-bar">
+      <img src="./img/2.png" alt="" />
+      <div>
+        <SearchBar></SearchBar>
+        <button className="hide-sidebar-button" onClick={toggleSidebar}>
+          <Icon icon="vaadin:lines" width="1.2em" height="1.2em" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
