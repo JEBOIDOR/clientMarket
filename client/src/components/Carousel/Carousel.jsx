7747f78a391391e5c853c6c403b3384012632a04
JEBOIDOR <juenexplo@gmail.com>
2024-02-12 20:57:18 -0500

    mejoras al about page

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,25 +1,32 @@
 import "./Carousel.css";
+import { useEffect, useState } from "react";
 
 const Carousel = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
-    <div className="carousel-container">
+    <div className={`carousel-container ${isLoaded ? "loaded" : ""}`}>
       <div className="carousel-content">
         <div className="section-container">
           <section className="section-left">
-            <img src="./img/setup.jpg" alt="" />
+            <img src="./img/sena.png" alt="" />
           </section>
           <section className="section-right">
             <h1>
               ABOUT <span className="title-text">US</span>
             </h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-              maxime cum blanditiis necessitatibus ipsam aperiam quisquam
-              placeat aspernatur sint minima, iure labore non quo quas
-              exercitationem sequi ad minus deserunt. <br />
-              <br /> possimus commodi, ut quae nobis. Rem excepturi qui at
-              fugiat deserunt dolorem, ipsa dolorum ipsam. Maxime quam laborum
-              dignissimos!
+              "Somos un equipo comprometido con la creación de una experiencia
+              única en el comercio electrónico. Nuestra plataforma combina la
+              conveniencia de las compras en línea con la interacción social,
+              conectando a usuarios, compradores y vendedores en un entorno
+              colaborativo. Buscamos mejorar la experiencia del usuario y
+              brindar oportunidades a pequeños vendedores para que prosperen en
+              el mercado digital."
             </p>
             <div className="button-container">
               <button className="button-contactUs">Contact us</button>
--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./TopBar.css";
+import { NavLink } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar.jsx";
 import { Icon } from "@iconify/react";
 const TopBar = ({ toggleSidebar }) => {
   return (
     <div className="top-bar">
-      <img src="./img/2.png" alt="" />
+      <NavLink to="/profileUser">
+        <img src="./img/2.png" alt="" className="img-top-bar" />
+      </NavLink>
 
       <SearchBar></SearchBar>
       <button className="hide-sidebar-button" onClick={toggleSidebar}>
